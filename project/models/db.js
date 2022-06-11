8a1e4419bfbb7e2e31942e80c7dffaebdb3ae436
svk1427 <75059407+svk1427@users.noreply.github.com>
2022-06-11 10:31:58 +0530

    Update db.js

--- project/models/db.js
+++ project/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb://0.0.0.0:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
